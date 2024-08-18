cf65efb90952fe452bcafbb385572ae59af842e5
putilovskaya <putilovskayaa@mail.ru>
2024-08-18 13:39:17 +0300

    create model users and prepare settings for model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "email",
+        "is_staff",
+        "is_superuser",
+    )
